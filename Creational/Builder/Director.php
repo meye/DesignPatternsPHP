996b0e5880b20a75ab19eae0cf904aff5ef7cfb8
Changwan Jun <me@wani.kr>
2015-04-23 14:41:45 +0900

    fix korean Creational/Builder

--- Creational/Builder/Director.php
+++ Creational/Builder/Director.php
@@ -3,9 +3,10 @@
 namespace DesignPatterns\Creational\Builder;
 
 /**
- * Director는 builder 패턴의 일부분입니다. Director는 builder 인터페이스를 알고 있으며, builder의 도움으로 복잡한 객체를 구축합니다.
+ * Director는 빌더 패턴의 일부분입니다. Director는 빌더 인터페이스를 알고 있으며, 빌더의 도움으로 복잡한 객체를
+ * 구축합니다.
  *
- * 당신은 또한 하나 이상의 복잡한 객체들을 구현하는 대신 믾은 builder들을 집어넣을 수 있습니다.
+ * 또한, 더 복잡한 객체들을 만드는 것 대신에 믾은 빌더들을 주입할 수 있습니다.
  */
 class Director
 {
