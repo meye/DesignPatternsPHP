879028708ef6728b6328a272bd58a5b9368a1e08
Changwan Jun <me@wani.kr>
2015-04-23 13:45:32 +0900

    fix korean Creational/StaticFactory

--- Creational/StaticFactory/StaticFactory.php
+++ Creational/StaticFactory/StaticFactory.php
@@ -3,8 +3,8 @@
 namespace DesignPatterns\Creational\StaticFactory;
 
 /**
- * Note1: 기억하세요, static => global => evil
- * Note2: 서브클래스, 모형(mock-up), 여러 인스턴스들을 가질 수 없습니다.
+ * Note1: 기억할것!, 정적(static) => 전역(global) => 좋지않음(evil)!
+ * Note2: 상속된 클래스, 목업(mock-up), 여러개의 인스턴스를 가질 수 없습니다.
  */
 class StaticFactory
 {
